fc33d28e3d17f695a366bd8f5c65b0dd91d6cb05
13212 <brynnstafford@gmail.com>
2020-09-04 00:14:59 -0400

    Update button outputs

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,14 +1,20 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
 
    @FXML
-   private Label lblOutput;
+   private Label lblOutput1;
+   @FXML
+   private Label lblOutput2;
 
-   public void sayHello() {
-      lblOutput.setText("Hello FXML!");
 
+   public void display1(ActionEvent actionEvent) {
+      lblOutput1.setText("(System.out.println)");
    }
 
+   public void display2(ActionEvent actionEvent) {
+      lblOutput2.setText("(System.out.println)");
+   }
 }
