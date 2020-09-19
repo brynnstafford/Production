a972bc6a4bca6c63571c76e3206c86f86a372f33
13212 <brynnstafford@gmail.com>
2020-09-19 13:45:00 -0400

    Update comments and readme

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,28 @@
+/******************************************************************************
+ *    File: io.github.brynnstafford.Controller.java
+ *    Date: 09/19/2020
+ *   Class: COP 3003
+ *  Author: B.Stafford
+ * Purpose: controller file for production project. Program permits
+ *          a user to add new products that are able to be produced and store
+ *          them in a collection. The collection of products that can be
+ *          produced can be displayed at any time. Allow a user to track
+ *          production of products, including specifying how many items of that
+ *          product were created. The program will then create a record for each
+ *          of these items and store them in a collection..
+ *******************************************************************************/
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+// controller manages the functions used to keep track of production log information
 public class Controller {
 
    @FXML
-   private Label lblOutput1;
+   private Label lblOutput1; // label output for add product
    @FXML
-   private Label lblOutput2;
+   private Label lblOutput2; // label output for record production
 
 
    public void display1(ActionEvent actionEvent) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+
+/******************************************************************************
+ *     File: io.github.brynnstafford.Main.java
+ *     Date: 09/19/2020
+ *    Class: COP 3003
+ *   Author: B.Stafford
+ *  Purpose: main application file for production project. Program permits
+ *           a user to add new products that are able to be produced and store
+ *           them in a collection. The collection of products that can be
+ *           produced can be displayed at any time. Allow a user to track
+ *           production of products, including specifying how many items of that
+ *           product were created. The program will then create a record for each
+ *           of these items and store them in a collection..
+ * Websites: https://sites.google.com/site/profvanselow/course/cop-3003/oop-project
+ *           https://sites.google.com/site/profvanselow/course/cop-3003/gui
+ *******************************************************************************/
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+//-----------------------------------------------------------------------------
+// Global space- constants, functions, data structures, etc.
+//-----------------------------------------------------------------------------
+
+// main calls functions to run Production log window
 public class Main extends Application {
 
    public static void main(String[] args) {
@@ -16,7 +39,7 @@ public class Main extends Application {
 
       Scene scene = new Scene(root, 300, 275);
 
-      primaryStage.setTitle("FXML Welcome");
+      primaryStage.setTitle("Production Project");
       primaryStage.setScene(scene);
       primaryStage.show();
    }
