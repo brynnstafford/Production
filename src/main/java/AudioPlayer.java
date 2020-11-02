27a77ae3c4a9a32424744530ae9b25a26258f03f
13212 <brynnstafford@gmail.com>
2020-11-02 11:53:42 -0500

    Issue 2: Create a class called AudioPlayer that is a subclass of Product and implements the MultimediaControl interface

---
+++ src/main/java/AudioPlayer.java
@@ -0,0 +1,24 @@
+public class AudioPlayer extends Product
+   implements MultimediaControl {
+
+
+   @Override
+   public void play() {
+
+   }
+
+   @Override
+   public void stop() {
+
+   }
+
+   @Override
+   public void previous() {
+
+   }
+
+   @Override
+   public void next() {
+
+   }
+}
