668d62d442b4db1164443c943fe2cb2028aee95b
13212 <brynnstafford@gmail.com>
2020-11-14 20:17:44 -0500

    Issue 3: ScreenSpec Interface created

---
+++ src/main/java/ScreenSpec.java
@@ -0,0 +1,7 @@
+// Create an interface called ScreenSpec.
+// This will define 3 methods:
+public interface ScreenSpec {
+   public String getResolution();
+   public int getRefreshRate();
+   public int getResponseTime();
+}
